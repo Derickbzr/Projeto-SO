45cc239b2565a584e99f9c0ca68f61e5c9958922
NiloAlmeidaaa <nilo15almeida@gmail.com>
2025-12-08 16:34:01 -0300

    main.py

--- PythonProjectSO/main.py
+++ PythonProjectSO/main.py
@@ -3,8 +3,7 @@ from master import Master
 from helpers import load_input
 
 def main():
-    # CONFIGURAÇÕES PADRÃO (modifique como quiser)
-    INPUT_FILE = "example_input.json"     # <-- coloque aqui o nome do arquivo JSON padrão
+    INPUT_FILE = "example_input.json"  
     POLICY = "RR"                 # opções: RR, SJF, PRIORITY
     ARRIVAL_MEAN = 0              # 0 = chegada imediata
     SEED = 42
@@ -36,3 +35,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
